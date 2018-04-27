55d992269875479f21b9489003eaa9bbdab0b0f4
blevm <mjblevins7@gmail.com>
2018-04-27 10:22:41 -0400

    Friday AM edits

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,6 +9,7 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
+    self.save
   end
 
   def add_song(song)
--- lib/song.rb
+++ lib/song.rb
@@ -10,16 +10,24 @@ class Song
   def self.new_by_filename(file)
     split_data = file.split((" -")).map(&:strip)
     imported_song = Song.new(split_data[1])
-    Song.artist_name=(split_data[0])
-    #binding.pry
-    puts "#{imported_song.name} was imported"
+    imported_song.artist = Song.artist_name=(split_data[0])
+    #puts "#{imported_song.name} was imported"
     imported_song
+    binding.pry
   end
 
   def self.artist_name=(name)
-    artist = Artist.find_or_create_by_name(name)
-    artist.add_song("It's Real")
-    artist.save
+    created_artist = Artist.find_or_create_by_name(name)
+
+    #artist.add_song("It's Real")
+    #binding.pry
+    if !Artist.all.include?(created_artist)
+      created_artist.save
+    end
+    return created_artist
+    #binding.pry
   end
+
+  #binding.pry
 
 end
